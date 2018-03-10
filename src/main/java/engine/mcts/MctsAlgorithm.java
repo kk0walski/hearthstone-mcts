767331c407a704a0124e226bedac0e89e6a6949b
jedrzej <jedrzej@Admin-Komputer.debian>
2018-03-10 10:44:16 +0000

    MCTS template implemented

--- src/main/java/engine/mcts/MctsAlgorithm.java
+++ src/main/java/engine/mcts/MctsAlgorithm.java
@@ -1,82 +1,130 @@
 package engine.mcts;
 
+import engine.Game;
 import engine.Move;
 import engine.cards.spells.Fireball;
 
 public class MctsAlgorithm {
-	
-	private static final int CP_FACTOR = 1;
-	private Node root;
 
-	public MctsAlgorithm(Node root) {
-		this.root = root;
-	}
-	
-	public Move run() {
-		long start = System.currentTimeMillis();
-		long end = start;
-		Move bestFound = null;
-		while(end - start >= 10 * 1000) {
-			Node child = treePolicy(root);
-			int delta = defaultPolicy(child);
-			backup(child, delta);
-			end = System.currentTimeMillis();
-		}
-		
-		return bestFound;
-	}
-	
-	Node treePolicy(Node root) {
-		while(nodeIsNonTerminal(root)) {
-			if(nodeIsNotFullyExpanded(root)) {
-				return expand(root);
-			} else {
-				root = bestChild(root, CP_FACTOR); // tu sie moze cos wykrzaczac z refkami
-				root.getMoveInNode().performMove();
-			}
-		}
-		return root;
-	}
-	
-	Node expand(Node root) {
-		Move move = root.getUntriedMoves().pop();
-		Node child = new Node(root, move);
-		root.addChild(child);
-		child.getMoveInNode().performMove();
-		return child;
-	}
-	
-	Node bestChild(Node root, int cFactor) {
-		return null; // TODO wzorek
-	}
-	
-	int defaultPolicy(Node root) {
-		while(nodeIsNonTerminal(root)) {
-			// TODO - kopiujemy ca³¹ grê (deep copy trzeba naklepaæ do ca³ego obiektu gry i w ogole wszystkiego
-			// i rozgrywamy gierke do konca i zwracamy 0 lub 1 w zaleznosci od tego czy wygrana
-		}
-	}
-	
-	void backup(Node root, int delta) {
-		while(root != null) {
-			if(root.getGame().getWinner() == root.getGame().getFirstHero()) {
-				root.addFirstHeroWin();
-			} else {
-				root.addSecondHeroWin();
-			}
-		}
-		
-		// winner = null
-		
-		// TODO - delta jest niepotrzebna, bo prowadzimy i tak gre do konca w defaultPolicy i w root.getGame().getWinner() mamy zwyciezce
-		// musimy tylko pamietac o tym, zeby rollbackowac winnera na null i gameOver na false i zeby przy rollbackowaniu ruchu EndRound zmieniac aktywnego gracza na przeciwnego
-	}
+    private static final int CP_FACTOR = 1;
+    private Node root;
 
-	private boolean nodeIsNotFullyExpanded(Node root) {
-		return !(root.getUntriedMoves().isEmpty());
-	}
+    public MctsAlgorithm(Node root) {
+        this.root = root;
+    }
 
-	private boolean nodeIsNonTerminal(Node root) {
-		return !(root.getGame().isGameOver());
-	}
+    public Move run() {
+        long start = System.currentTimeMillis();
+        long end = start;
+        Move bestFound = null;
+        while (end - start >= 10 * 1000) {
+            Node child = treePolicy(root);
+            int delta = defaultPolicy(child);
+            backup(child, delta);
+            end = System.currentTimeMillis();
+        }
+
+        return bestFound;
+    }
+
+    Node treePolicy(Node root) {
+        while (nodeIsNonTerminal(root)) {
+            if (nodeIsNotFullyExpanded(root)) {
+                return expand(root);
+            } else {
+                root = bestChild(root, CP_FACTOR); // tu sie moze cos wykrzaczac z refkami
+                root.getMoveInNode().performMove();
+            }
+        }
+        return root;
+    }
+
+    Node expand(Node root) {
+        Move move = root.getUntriedMoves().pop();
+        Node child = new Node(root, move);
+        root.addChild(child);
+        child.getMoveInNode().performMove();
+        return child;
+    }
+
+    Node bestChild(Node root, int cFactor) {
+        Node bestChild = null;
+        double bestFoundChildFormulaValue = Integer.MIN_VALUE;
+        for(Node child: root.getChilds()) {
+            double current = evaluateBestChildFormula(child, cFactor);
+            if(current > bestFoundChildFormulaValue) {
+                bestChild = child;
+            }
+        }
+        return bestChild;
+    }
+
+    private double evaluateBestChildFormula(Node child, int cFactor) {
+        int q = 0;
+
+        if (child.getGame().getFirstHero() == child.getGame().getActiveHero()) {
+            q = child.getFirstHeroWins();
+        } else {
+            q = child.getSecondHeroWins();
+        }
+
+        if (child.getTotalGames() == 0) {
+            return Integer.MAX_VALUE;
+        }
+
+        return q / child.getTotalGames() + cFactor * Math.sqrt(
+                        (2 * Math.log(child.getParent().getTotalGames()))
+                                / child.getTotalGames()
+        );
+    }
+
+    int defaultPolicy(Node root) {
+
+        Game copy = new Game();
+        while (nodeIsNonTerminal(root)) {
+            // TODO - kopiujemy caï¿½ï¿½ grï¿½ (deep copy trzeba naklepaï¿½ do caï¿½ego obiektu gry i w ogole wszystkiego
+            // TODO - i rozgrywamy gierke do konca i zwracamy 0 lub 1 w zaleznosci od tego czy wygrana
+        }
+        if(root.getGame().getActiveHero().equals(copy.getWinner())) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * If detla == 1, then activeHero in input node has won.
+     * If delta == -1, then activeHero in input node has lose.
+     *
+     * @param nodeToBackup input node
+     * @param delta binary value of simulation (defaultPolicy) result
+     */
+    void backup(Node nodeToBackup, int delta) {
+        if(nodeToBackup.getGame().isGameOver()) {
+            nodeToBackup.getGame().setGameOver(false);
+            nodeToBackup.getGame().setWinner(null);
+        }
+        while (nodeToBackup != null) {
+            if (delta == 1) {
+                nodeToBackup.addFirstHeroWin();
+            } else {
+                nodeToBackup.addSecondHeroWin();
+            }
+
+            nodeToBackup.getMoveInNode().rollback();
+
+            nodeToBackup = nodeToBackup.getParent();
+        }
+
+        // TODO - prowadzimy gre do konca w defaultPolicy i w root.getGame().getWinner() mamy zwyciezce
+        // TODO -  musimy tylko pamietac o tym, zeby przy rollbackowaniu ruchu EndRound zmieniac aktywnego gracza na przeciwnego
+    }
+
+    private boolean nodeIsNotFullyExpanded(Node root) {
+        return !(root.getUntriedMoves().isEmpty());
+    }
+
+    private boolean nodeIsNonTerminal(Node root) {
+        return !(root.getGame().isGameOver());
+    }
 }
